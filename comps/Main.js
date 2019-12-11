e1a2bdae5909fa15e261f338064971f234c4f28b
Nicole <nicole.ll.930529@gmail.com>
2019-12-10 20:49:06 -0800

    story book update

--- comps/Main.js
+++ comps/Main.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import Animation_Ang from './Animation_Ang';
 
+
 function Main(){
     return(
         <View>
             <Animation_Ang/>
+           
         </View>
     )
 }
