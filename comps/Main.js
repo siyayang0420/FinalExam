43a776023aaf3e343b3f776073b822586dd1f288
AngelaYJ <yj.angela.jang@gmail.com>
2019-12-10 19:48:33 -0800

    Check my animation
    I finished animation, check it plz

--- comps/Main.js
+++ comps/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Animation from './Animation';
 
 function Main(){
     return(
         <View>
-            
+            <Animation/>
         </View>
     )
 }
