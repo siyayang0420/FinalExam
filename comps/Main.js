3d045fcb289c2251b988ea8ce1dfc6354aa6dd63
siyayang0420 <siyayang0420@live.com>
2019-12-10 20:28:43 -0800

    added nicoles's

--- comps/Main.js
+++ comps/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import Animation_Ang from './Animation_Ang';
+import Animation_N from './Animation_N';
 
 function Main(){
     return(
         <View>
+            <Animation_N/>
             <Animation_Ang/>
         </View>
     )
