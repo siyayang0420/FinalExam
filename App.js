7cf588a8e70b0f8eed1e7f1c9fb824703771e392
siyayang0420 <siyayang0420@live.com>
2019-12-10 18:46:47 -0800

    Initial set up

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Main from './comps/Main'
+
+function App(){
+  return(
+    <View>
+      <Main/>
+    </View>
+  )
+}
+
+export default App;
